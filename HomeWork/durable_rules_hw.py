2f5c2b9abacf56b340a44e072fec5c7e4d78bc92
HEEKWON <higun295@gmail.com>
2024-03-31 11:35:09 +0900

    add new ruleset

--- HomeWork/durable_rules_hw.py
+++ HomeWork/durable_rules_hw.py
@@ -6,11 +6,9 @@ with ruleset('ssm_application_rules'):
         print(f"사원 '{c.m.name}'은 스마트폰에 SSM 어플리케이션을 설치 해야 합니다.")
         assert_fact('ssm_application_rules', {'name': c.m.name, 'action': 'collect_info'})
 
-
     @when_all(m.action == 'collect_info')
     def collect_device_info(c):
         print(f"회사에서 사원 '{c.m.name}'의 스마트폰 하드웨어 정보와 전화번호를 수집합니다.")
-
 
     @when_all(m.role == 'employee')
     def apply_security_sticker(c):
@@ -38,20 +36,22 @@ with ruleset('campus_security_rules'):
     def photo_violation(c):
         print(f"사원 '{c.photo.name}'이 캠퍼스 내에서 촬영을 시도했습니다.")
         assert_fact('campus_security_rules',
-                    {'name': c.photo.name, 'role': 'security_guard', 'action': 'inspect_smartphone'})
-
+                    {'name': c.photo.name, 'role': 'security_guard', 'action': 'inspect_smartphone', 'photo_saved': c.photo.photo_saved})
 
     @when_all((m.role == 'security_guard') & (m.action == 'inspect_smartphone'))
     def inspect_smartphone(c):
         print(f"보안 요원이 사원 '{c.m.name}'의 스마트폰을 점검하고 있습니다.'")
-        if (c.m.role == 'security_guard') & (c.action == 'find_violation_evidence'):
+        if (c.m.role == 'security_guard') & (c.m.photo_saved == 'true'):
             print(f"보안 요원이 사원 '{c.m.name}'의 스마트폰에서 촬영 기록을 확인했습니다. 보안 위규 처리 되었습니다.")
         else:
             print(f"보안 요원이 사원 '{c.m.name}'의 스마트폰에서 촬영 기록을 확인하지 못했습니다.")
+
+with ruleset(''):
+    
 
 print('========SSM 어플리케이션 보안 규정========')
 assert_fact('ssm_application_rules', {'role': 'employee', 'name': '신희권', 'isCommunicationPossible': 'true'})
 print('========캠퍼스 내 보안 규정========')
 assert_fact('campus_security_rules', {'role': 'employee', 'name': '신희권', 'taggingLocation': 'main_gate_outside'})
 assert_fact('campus_security_rules', {'role': 'employee', 'name': '신희권', 'taggingLocation': 'security_gate_outside'})
-assert_fact('campus_security_rules', {'name': '신희권', 'action': 'take_photo', 'current_location': 'inside_campus'})
+assert_fact('campus_security_rules', {'name': '신희권', 'action': 'take_photo', 'current_location': 'inside_campus', 'photo_saved': 'true'})
