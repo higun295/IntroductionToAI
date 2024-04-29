c96081efa976d99e8f3e7af7d593e8bbdb67371a
HEEKWON <higun295@gmail.com>
2024-04-29 20:17:32 +0900

    test

--- pythonProject/test.py
+++
@@ -1,9 +0,0 @@
-from durable.lang import *
-
-with ruleset('testRS'):
-    @when_all((m.subject == 'World') & (m.body == 'Hello'))
-    def say_hello(c):
-        print('Hello {0}'.format(c.m.subject))
-
-result = post('testRS', {'subject': 'World', 'body': 'Hello'})
-print(result)
